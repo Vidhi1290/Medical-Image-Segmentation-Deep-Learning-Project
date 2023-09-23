83ed0956c87d4526aa82b6f9da0914c4cb0cb55f
Vidhi Waghela <vidhiwaghela60@gmail.com>
2023-09-23 18:46:49 +0530

    Initial commit

---
+++ Code/source/ML_Pipeline/utils.py
@@ -0,0 +1,34 @@
+import torch
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        
+def iou_score(output, target):
+    smooth = 1e-5
+
+    if torch.is_tensor(output):
+        output = torch.sigmoid(output).data.cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+    output_ = output > 0.5
+    target_ = target > 0.5
+    intersection = (output_ & target_).sum()
+    union = (output_ | target_).sum()
+
+    return (intersection + smooth) / (union + smooth)
---
+++ Code/source/README.md
@@ -0,0 +1,11 @@
+
+### create a new environment 
+
+### Installation
+
+To install the dependencies run:
+```
+pip install -r requirements.txt
+```
+
+### run the engine.py file
